b80a8f6382d8d2bd41ae9fe663f03efa134642b2
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-11-08 06:25:04 -0800

    WK6Lab1-Invoice Line Item Calculator

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -5,4 +5,32 @@
 def get_price():
     while True:
         try:
-            price = float
+            price = float(input("Enter price: "))
+            return price
+        except ValueError:
+            print("Invalid decimal number. Please try again. ")
+def get_quantity():
+    while True:
+        try:
+            quantity = int(input("Enter quantity: "))
+            return quantity
+        except ValueError:
+            print("Invalid integer. Please try again. ")
+def main():
+    print("The Invoice Line Item Calculater\n")
+
+    answer = "y"
+    while answer.lower() == "y":
+        price = get_price()
+        quantity = get_quantity()
+        total = price * quantity
+        print()
+        print("PRICE: ", f"{price: .2f}")
+        print("QUANTITY: ", quantity)
+        print("TOTAL:  ", f"{total: .2f}")
+        answer = input("Enter another line item? (y/n): ")
+        print()
+        print("Bye!")
+
+if __name__ == "__main__":
+    main()
